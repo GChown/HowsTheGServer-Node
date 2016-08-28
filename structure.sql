4b5e2b66b627c96c301c752ba1179ca3ed58d4ed
Gordie Chown <gordie.chown@gmail.com>
2016-08-28 10:41:59 -0400

    Update SQL structure to allow emojis, fix package

--- structure.sql
+++ structure.sql
@@ -1,33 +1,28 @@
-use HowsTheGdb;
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `U_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `device` varchar(255) NOT NULL,
-  `usrname` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`U_ID`),
-  UNIQUE KEY `device` (`device`)
-) ENGINE=InnoDB AUTO_INCREMENT=259 DEFAULT CHARSET=latin1;
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
   `C_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `text` varchar(255) NOT NULL,
-  `numvote` int(11) NOT NULL DEFAULT '1',
-  `timesent` datetime NOT NULL DEFAULT '2016-01-14 13:05:23',
+  `googleid` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `numvote` int(11) DEFAULT NULL,
+  `timesent` datetime DEFAULT NULL,
   PRIMARY KEY (`C_ID`),
-  KEY `uid` (`uid`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`U_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `googleid` (`googleid`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`googleid`) REFERENCES `user` (`googleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `googleid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`googleid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 DROP TABLE IF EXISTS `vote`;
 CREATE TABLE `vote` (
   `V_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
+  `googleid` varchar(255) DEFAULT NULL,
   `vote_b` int(11) DEFAULT NULL,
   `vote_l` int(11) DEFAULT NULL,
   `vote_d` int(11) DEFAULT NULL,
-  `lastvote` datetime NOT NULL,
+  `date` date DEFAULT NULL,
   PRIMARY KEY (`V_ID`),
-  KEY `uid` (`uid`),
-  CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`U_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `unique` (`googleid`,`date`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
